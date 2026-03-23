78d1bd3b747fafa55d856f1c8ae6102eb9da0423
Chen Beitian <selena.cbt0102@gmail.com>
2026-03-23 18:27:46 +0800

    Refactor FindDeliveryCommandParser to use prefixes for date and date range search

--- src/main/java/seedu/address/logic/parser/FindDeliveryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindDeliveryCommandParser.java
@@ -1,6 +1,11 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FIND_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FIND_END;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FIND_START;
+
+import java.time.LocalDate;
 
 import seedu.address.logic.commands.FindDeliveryCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -11,6 +16,9 @@ import seedu.address.model.delivery.DeliveryDatePredicate;
  */
 public class FindDeliveryCommandParser implements Parser<FindDeliveryCommand> {
 
+    public static final String MESSAGE_START_AFTER_END =
+            "Start date must not be after end date.";
+
     /**
      * Parses the given {@code String} of arguments in the context of the
      * FindDeliveryCommand and returns a FindDeliveryCommand object for execution.
@@ -19,14 +27,40 @@ public class FindDeliveryCommandParser implements Parser<FindDeliveryCommand> {
      */
     public FindDeliveryCommand parse(String args) throws ParseException {
 
-        String trimmedArgs = args.trim();
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_FIND_DATE, PREFIX_FIND_START, PREFIX_FIND_END);
 
-        if (trimmedArgs.isEmpty()) {
+        if (!argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                            FindDeliveryCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
         }
 
-        return new FindDeliveryCommand(new DeliveryDatePredicate(ParserUtil.parseDate(trimmedArgs)));
+        boolean hasDate = argMultimap.getValue(PREFIX_FIND_DATE).isPresent();
+        boolean hasStart = argMultimap.getValue(PREFIX_FIND_START).isPresent();
+        boolean hasEnd = argMultimap.getValue(PREFIX_FIND_END).isPresent();
+
+        if (hasDate && (hasStart || hasEnd)) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
+        }
+
+        if (hasDate) {
+            argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_FIND_DATE);
+            LocalDate date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_FIND_DATE).get());
+            return new FindDeliveryCommand(new DeliveryDatePredicate(date));
+        }
+
+        if (hasStart && hasEnd) {
+            argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_FIND_START, PREFIX_FIND_END);
+            LocalDate startDate = ParserUtil.parseDate(argMultimap.getValue(PREFIX_FIND_START).get());
+            LocalDate endDate = ParserUtil.parseDate(argMultimap.getValue(PREFIX_FIND_END).get());
+            if (startDate.isAfter(endDate)) {
+                throw new ParseException(MESSAGE_START_AFTER_END);
+            }
+            return new FindDeliveryCommand(new DeliveryDatePredicate(startDate, endDate));
+        }
+
+        throw new ParseException(
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
     }
 }
--- src/test/java/seedu/address/logic/parser/FindDeliveryCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindDeliveryCommandParserTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.FindDeliveryCommandParser.MESSAGE_START_AFTER_END;
 import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_DATE;
 
 import java.time.LocalDate;
@@ -23,20 +24,87 @@ public class FindDeliveryCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindDeliveryCommand() {
-
-        LocalDate date = LocalDate.parse("2026-04-01");
-
-        FindDeliveryCommand expectedCommand =
-                new FindDeliveryCommand(new DeliveryDatePredicate(date));
-
-        assertParseSuccess(parser, "2026-04-01", expectedCommand);
-
-        assertParseSuccess(parser, " \n 2026-04-01 \t", expectedCommand);
+    public void parse_noPrefixes_throwsParseException() {
+        // raw date with no prefix
+        assertParseFailure(parser, "2026-04-01",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_invalidDate_throwsParseException() {
-        assertParseFailure(parser, "invalid-date", MESSAGE_INVALID_DATE);
+    public void parse_validSingleDate_returnsFindDeliveryCommand() {
+        LocalDate date = LocalDate.parse("2026-04-01");
+        FindDeliveryCommand expectedCommand =
+                new FindDeliveryCommand(new DeliveryDatePredicate(date));
+
+        // exact input
+        assertParseSuccess(parser, " d/2026-04-01", expectedCommand);
+
+        // extra whitespace around date value
+        assertParseSuccess(parser, " d/ 2026-04-01 ", expectedCommand);
+    }
+
+    @Test
+    public void parse_validDateRange_returnsFindDeliveryCommand() {
+        LocalDate startDate = LocalDate.parse("2026-04-01");
+        LocalDate endDate = LocalDate.parse("2026-04-30");
+        FindDeliveryCommand expectedCommand =
+                new FindDeliveryCommand(new DeliveryDatePredicate(startDate, endDate));
+
+        assertParseSuccess(parser, " s/2026-04-01 e/2026-04-30", expectedCommand);
+    }
+
+    @Test
+    public void parse_sameDateRange_returnsFindDeliveryCommand() {
+        // s/ and e/ on the same date should equal the single-date predicate
+        LocalDate date = LocalDate.parse("2026-04-01");
+        FindDeliveryCommand expectedCommand =
+                new FindDeliveryCommand(new DeliveryDatePredicate(date));
+
+        assertParseSuccess(parser, " s/2026-04-01 e/2026-04-01", expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidSingleDate_throwsParseException() {
+        assertParseFailure(parser, " d/invalid-date", MESSAGE_INVALID_DATE);
+    }
+
+    @Test
+    public void parse_invalidStartDate_throwsParseException() {
+        assertParseFailure(parser, " s/invalid-date e/2026-04-30", MESSAGE_INVALID_DATE);
+    }
+
+    @Test
+    public void parse_invalidEndDate_throwsParseException() {
+        assertParseFailure(parser, " s/2026-04-01 e/invalid-date", MESSAGE_INVALID_DATE);
+    }
+
+    @Test
+    public void parse_startAfterEnd_throwsParseException() {
+        assertParseFailure(parser, " s/2026-04-30 e/2026-04-01", MESSAGE_START_AFTER_END);
+    }
+
+    @Test
+    public void parse_missingEndDate_throwsParseException() {
+        // s/ without e/
+        assertParseFailure(parser, " s/2026-04-01",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingStartDate_throwsParseException() {
+        // e/ without s/
+        assertParseFailure(parser, " e/2026-04-30",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_mixedPrefixes_throwsParseException() {
+        // d/ mixed with s/
+        assertParseFailure(parser, " d/2026-04-01 s/2026-04-01",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
+
+        // d/ mixed with e/
+        assertParseFailure(parser, " d/2026-04-01 e/2026-04-30",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindDeliveryCommand.MESSAGE_USAGE));
     }
 }
